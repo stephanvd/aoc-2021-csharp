917474658e2f35d86c3d683e98794cbb861d8cbb
Stephan van Diepen <stephan@zilverline.com>
2021-12-02 11:23:28 +0100

    Day 1: Clean up

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,7 +1,14 @@
-﻿var measurements = new MeasurementReader("input").Read();
-var increments = new IncrementCounter(measurements).Count();
-Console.WriteLine($"Counted increments: {increments}");
+﻿var measurements = File.ReadAllText("input")
+  .Split('\n')
+  .ToList()
+  .FindAll(line => !string.IsNullOrWhiteSpace(line))
+  .Select(int.Parse);
 
-var slidingWindow = new SlidingWindow(measurements).GroupsOf(3);
-var slidingIncrements = new IncrementCounter(slidingWindow).Count();
-Console.WriteLine($"Sliding increments: {slidingIncrements}");
+var threeMeasurementSlidingWindow = measurements.Skip(2)
+  .Zip(measurements.Skip(1), (curr, prev) => curr + prev)
+  .Zip(measurements, (curr, prev) => curr + prev);
+
+var countIncrements = (IEnumerable<int> m) => m.Skip(1).Zip(m, (c, p) => c > p).Count(b => b);
+
+Console.WriteLine($"Part 1: {countIncrements(measurements)}");
+Console.WriteLine($"Part 2: {countIncrements(threeMeasurementSlidingWindow)}");
