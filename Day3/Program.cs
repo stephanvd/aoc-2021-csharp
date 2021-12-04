b36cef70e91cc8d9b058bd7c38338275b95b3ae0
Stephan van Diepen <stephan@zilverline.com>
2021-12-04 16:42:45 +0100

    Day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,19 +1,28 @@
 ﻿// https://adventofcode.com/2021/day/3
 
-var report = File.ReadAllText("input")
+var lines = File.ReadAllText("input")
   .Split('\n')
   .ToList()
-  .FindAll(line => !string.IsNullOrWhiteSpace(line))
-  .Select(line => line.ToCharArray().Select(char.ToString).Select(int.Parse).ToList())
-  .ToList();
+  .FindAll(line => !string.IsNullOrWhiteSpace(line));
 
-var bits = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-var sums = report.Aggregate(bits, (acc, item) => acc.Zip(item, (a, i) => a + i).ToList());
+var bits = Enumerable.Range(0, lines[0].Length);
 
-var result = (Func<int, char> fn) =>  Convert.ToInt64(new string(sums.Select(fn).ToArray()), 2);
-var count = report.Count();
-var gamma = result(sum => sum > count / 2 ? '1' : '0');
-var epsilon = result(sum => sum < count / 2 ? '1' : '0');
+var mostCommon = (List<string> lines, int i) => lines.Count(line => line[i] == '1') * 2 >= lines.Count() ? '1' : '0';
+var leastCommon = (List<string> lines, int i) => mostCommon(lines, i) == '1' ? '0' : '1';
 
-Console.WriteLine($"Part 1: {gamma * epsilon}"); // Answer: 1131506
+var calculateRate = (List<string> lines, Func<List<string>, int, char> selector) =>
+  bits.Select(i => selector(lines, i)).Aggregate("", (acc, item) => acc += item);
 
+var gammaRate = Convert.ToInt32(calculateRate(lines, mostCommon), 2);
+var epsilonRate = Convert.ToInt32(calculateRate(lines, leastCommon), 2);
+
+Console.WriteLine($"Part 1: {gammaRate * epsilonRate}");
+
+var findRate = (List<string> lines, Func<List<string>, int, char> selector) => bits
+  .Aggregate(lines, (acc, i) => acc.Count() == 1 ? acc : acc.FindAll(line => line[i] == selector(acc, i)))
+  .First();
+
+var oxygenRate = Convert.ToInt32(findRate(lines, mostCommon), 2);
+var co2Rate = Convert.ToInt32(findRate(lines, leastCommon), 2);
+
+Console.WriteLine($"Part 2: {oxygenRate * co2Rate}");
